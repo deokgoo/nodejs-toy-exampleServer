b517c98f3859f1e131504be70d3172185fe26f2b
deokgoo <kkddgg1001@gmail.com>
2020-04-05 22:24:13 +0900

    init data

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
@@ -34,23 +35,25 @@ const User = sequelize.define('auths', {
   modelName: 'auths'
 });
 
-User.sync({force: true});
-
 export const createUser = (data) => {
   return User.create(data);
 };
-
-const salt = "custom_salt";
-createUser({
-  id: 'deok',
-  pw: cryptoPW(salt, 'deok'),
-  name: 'admin',
-  email: 'testEmail',
-  salt
-});
 
 export const findUser = (id) => {
   return User.findOne({
     where: {id}
   });
 };
+
+User.sync({force: true}).then(() => {
+  const salt = "custom_salt";
+  let hash = crypto.createHmac('sha256', salt);
+  createUser({
+    id: 'deok',
+    pw: hash.update('deok')
+            .digest("base64"),
+    name: 'admin',
+    email: 'testEmail',
+    salt
+  });
+})
